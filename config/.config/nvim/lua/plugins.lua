b68d84a14fd22c15549e0d816a30a6c33d555464
Oleksii Lisikh <o.lisikh@newmotion.com>
2023-05-14 23:01:50 +0200

    Formatting

--- config/.config/nvim/lua/plugins.lua
+++ config/.config/nvim/lua/plugins.lua
@@ -28,7 +28,11 @@ return require('lazy').setup({
   -- Autocompletion
   {
     'hrsh7th/nvim-cmp',
-    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+    },
   },
 
   -- NOTE: This is where your plugins related to LSP can be installed.
