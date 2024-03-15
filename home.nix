2f92adb4e4f626a8e966ae3c1dadef809cf8c0a0
Oleksii Lisikh <o.lisikh@shellrecharge.com>
2024-03-15 19:42:11 +0100

    Show folder name as window in tmux

--- home.nix
+++ home.nix
@@ -307,18 +307,19 @@ in
         plugin = catppuccin;
         extraConfig = ''
           set -g @catppuccin_flavour ${catppuccinFlavour}
+
           set -g @catppuccin_window_left_separator ""
           set -g @catppuccin_window_right_separator " "
           set -g @catppuccin_window_middle_separator "█ "
           set -g @catppuccin_window_number_position "left"
 
           set -g @catppuccin_window_default_fill "number"
-          set -g @catppuccin_window_default_text "#W"
-
+          set -g @catppuccin_window_default_text "#{b:pane_current_path}"
           set -g @catppuccin_window_current_fill "number"
-          set -g @catppuccin_window_current_text "#W"
+          set -g @catppuccin_window_current_text "#{b:pane_current_path}"
 
-          set -g @catppuccin_status_modules_right "directory session"
+          set -g @catppuccin_status_default "off"
+          set -g @catppuccin_status_modules_right "directory application session"
           set -g @catppuccin_status_left_separator  " "
           set -g @catppuccin_status_right_separator ""
           set -g @catppuccin_status_right_separator_inverse "no"
