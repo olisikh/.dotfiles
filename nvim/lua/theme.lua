530dc2f5e1ebf2bcb9fdc2a1ed7488b875ce4798
Oleksii Lisikh <alisiikh@gmail.com>
2024-03-16 03:01:40 +0100

    Hide fileformat item from lualine

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -76,6 +76,12 @@ require('catppuccin').setup({
 require('lualine').setup({
   options = {
     theme = 'catppuccin',
+
+    section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
+  },
+  sections = {
+    lualine_x = { 'encoding', 'filetype' },
   },
 })
 
