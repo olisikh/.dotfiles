6ad1740923ef9a9fea48704d4db65f31bd4acf1b
Oleksii Lisikh <alisiikh@gmail.com>
2024-04-03 02:01:56 +0200

    Remove playground

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -132,7 +132,6 @@ require('lazy').setup({
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
       'nvim-treesitter/nvim-treesitter-context',
-      'nvim-treesitter/playground',
     },
     config = function()
       pcall(require('nvim-treesitter.install').update({ with_sync = true }))
---
+++ nvim/queries/lua/injections.scm
@@ -0,0 +1,5 @@
+;treesitter query as an argument in a function
+(_
+  (comment content: (comment_content) @injection.language) 
+  (string (string_content) @injection.content)
+)
