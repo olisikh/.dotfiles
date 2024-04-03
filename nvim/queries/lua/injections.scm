ec00d8ad39b259db0b730287d3a06b6e99c439db
Oleksii Lisikh <alisiikh@gmail.com>
2024-04-04 00:06:48 +0200

    Language injections for lua

--- nvim/queries/lua/injections.scm
+++ nvim/queries/lua/injections.scm
@@ -1,5 +1,28 @@
-;treesitter query as an argument in a function
+;a string passed as an argument to a function with a comment on top
 (_
   (comment content: (comment_content) @injection.language) 
   (string (string_content) @injection.content)
+)
+
+;variable string with a comment on top
+(
+  (comment
+    content: (comment_content) @injection.language
+  )
+  (variable_declaration
+    (assignment_statement
+      (_)
+      (expression_list
+        value: (string
+          content: (string_content) @injection.content
+        )
+      )
+    )
+  )
+)
+
+;a field of table with a comment on top
+(table_constructor
+  (comment content: (comment_content) @injection.language)
+  (field name: (_) value: (string (string_content) @injection.content))
 )
