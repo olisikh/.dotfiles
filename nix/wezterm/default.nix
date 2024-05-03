dbab833cbadfdcd4f7b38e9bf890d95888d61a6b
Oleksii Lisikh <alisiikh@gmail.com>
2024-05-03 21:39:48 +0200

    Fix formatting in wezterm/default.nix

--- nix/wezterm/default.nix
+++ nix/wezterm/default.nix
@@ -16,12 +16,12 @@
       ''
         return {
           color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
-            keys = {
-             -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
-             {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
-             -- Make Option-Right equivalent to Alt-f; forward-word
-             {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
-            }
+          keys = {
+            -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+            {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+            -- Make Option-Right equivalent to Alt-f; forward-word
+            {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+          }
         }
       '';
   };
