afe8d9eb31284891183eed2ce067dff9ee6146f5
Oleksii Lisikh <alisiikh@gmail.com>
2024-05-04 01:59:11 +0200

    Remove tmux config from fzf

--- nix/fzf/default.nix
+++ nix/fzf/default.nix
@@ -24,8 +24,5 @@
         "--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc"
         "--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
       ];
-    tmux = {
-      enableShellIntegration = true;
-    };
   };
 }
