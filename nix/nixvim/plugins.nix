2c6159425ab10e3970ef3ce153a3b20a453511a9
Oleksii Lisikh <alisiikh@gmail.com>
2024-12-30 04:02:54 +0100

    Configure lsps, fix blink config using raw luaConfig prop

--- nix/nixvim/plugins.nix
+++ nix/nixvim/plugins.nix
@@ -280,83 +280,76 @@
 
     blink-cmp = {
       enable = true;
+      luaConfig = {
+        pre =
+          # lua
+          ''
+            require('blink.cmp').setup({
+              -- 'default' for mappings similar to built-in completion
+              -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+              -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+              -- See the full "keymap" documentation for information on defining your own keymap.
+              keymap = { preset = 'default' },
 
-      settings = {
-        keymap = {
-          # -- 'default' for mappings similar to built-in completion
-          # -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-          # -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-          # -- See the full "keymap" documentation for information on defining your own keymap.
-          preset = "default";
-        };
+              -- Default list of enabled providers defined so that you can extend it
+              -- elsewhere in your config, without redefining it, due to `opts_extend`
+              sources = {
+                default = { 'lazydev', 'lsp', 'path', 'snippets' },
+                providers = {
+                  lazydev = {
+                    name = 'LazyDev',
+                    module = 'lazydev.integrations.blink',
+                    score_offset = 100, -- show at a higher priority than lsp
+                  },
+                },
+              },
 
-        appearance = {
-          # -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-          # -- Useful for when your theme doesn't support blink.cmp
-          # -- Will be removed in a future release
-          use_nvim_cmp_as_default = false;
+              appearance = {
+                -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+                -- Useful for when your theme doesn't support blink.cmp
+                -- Will be removed in a future release
+                use_nvim_cmp_as_default = false,
+                -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+                -- Adjusts spacing to ensure icons are aligned
+                nerd_font_variant = 'mono',
+              },
 
-          # -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-          # -- Adjusts spacing to ensure icons are aligned
-          nerd_font_variant = "mono";
-        };
+              completion = {
+                -- 'prefix' will fuzzy match on the text before the cursor
+                -- 'full' will fuzzy match on the text before *and* after the cursor
+                -- example: 'foo_|_bar' will match 'foo_' for 'prefix' and 'foo__bar' for 'full'
+                keyword = { range = 'full' },
 
-        sources = {
-          default = [
-            "lazydev"
-            "lsp"
-            "path"
-            "snippets"
-            "buffer"
-          ];
-        };
+                -- Disable auto brackets
+                -- NOTE: some LSPs may add auto brackets themselves anyway
+                accept = { auto_brackets = { enabled = false } },
 
-        completion = {
-          # -- 'prefix' will fuzzy match on the text before the cursor
-          # -- 'full' will fuzzy match on the text before *and* after the cursor
-          # -- example: 'foo_|_bar' will match 'foo_' for 'prefix' and 'foo__bar' for 'full'
-          keyword = {
-            range = "full";
-          };
+                -- Insert completion item on selection, don't select by default
+                list = { selection = 'auto_insert' },
 
-          # -- Disable auto brackets
-          # -- NOTE: some LSPs may add auto brackets themselves anyway
-          accept = {
-            auto_brackets = {
-              enabled = false;
-            };
-          };
+                menu = {
+                  -- Don't automatically show the completion menu
+                  auto_show = false,
 
-          # -- Insert completion item on selection, don't select by default
-          list = { selection = "auto_insert"; };
+                  -- nvim-cmp style menu
+                  draw = {
+                    columns = {
+                      { 'label', 'label_description', gap = 1 },
+                      { 'kind_icon', 'kind', gap = 1 },
+                    },
+                  },
+                },
 
-          menu = {
-            # -- Don't automatically show the completion menu
-            auto_show = false;
+                -- Show documentation when selecting a completion item
+                documentation = { auto_show = true, auto_show_delay_ms = 500 },
 
-            # -- nvim-cmp style menu
-            # TODO: figure how to fix this
-            draw = {
-              # columns = [
-              #   [ "label" "label_description" { gap = 1; } ]
-              #   [ "kind_icon" "kind" ]
-              # ];
-            };
-          };
+                -- Display a preview of the selected item on the current line
+                ghost_text = { enabled = true },
+              },
 
-          # -- Show documentation when selecting a completion item
-          documentation = {
-            auto_show = true;
-            auto_show_delay_ms = 200;
-          };
-
-          # -- Display a preview of the selected item on the current line
-          ghost_text = { enabled = true; };
-        };
-
-        signature = {
-          enabled = true;
-        };
+              -- Experimental signature help support
+              signature = { enabled = true },
+            })'';
       };
     };
 
@@ -364,14 +357,76 @@
       enable = true;
       inlayHints = true;
       servers = {
-        lua_ls.enable = true;
+        lua_ls = {
+          enable = true;
+          settings = {
+            Lua = {
+              workspace = {
+                checkThirdParty = false;
+              };
+              telemetry = {
+                enable = false;
+              };
+              format = {
+                enable = false;
+              };
+              hint = {
+                enable = true;
+              };
+            };
+          };
+        };
         dockerls.enable = true;
         bashls.enable = true;
         html.enable = true;
         cssls.enable = true;
         tailwindcss.enable = true;
-        ts_ls.enable = true;
-        gopls.enable = true;
+        ts_ls = {
+          enable = true;
+          settings = {
+            javascript = {
+              inlayHints = {
+                includeInlayEnumMemberValueHints = true;
+                includeInlayFunctionLikeReturnTypeHints = true;
+                includeInlayFunctionParameterTypeHints = true;
+                includeInlayParameterNameHints = "literals"; # -- 'none' | 'literals' | 'all';
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false;
+                includeInlayPropertyDeclarationTypeHints = true;
+                includeInlayVariableTypeHints = false;
+                includeInlayVariableTypeHintsWhenTypeMatchesName = false;
+              };
+            };
+            typescript = {
+              inlayHints = {
+                includeInlayEnumMemberValueHints = true;
+                includeInlayFunctionLikeReturnTypeHints = true;
+                includeInlayFunctionParameterTypeHints = true;
+                includeInlayParameterNameHints = "literals"; # -- 'none' | 'literals' | 'all';
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false;
+                includeInlayPropertyDeclarationTypeHints = true;
+                includeInlayVariableTypeHints = false;
+                includeInlayVariableTypeHintsWhenTypeMatchesName = false;
+              };
+            };
+          };
+        };
+        gopls = {
+          enable = true;
+          settings = {
+            gopls = {
+              # -- setup inlay hints
+              hints = {
+                assignVariableTypes = true;
+                compositeLiteralFields = true;
+                compositeLiteralTypes = true;
+                constantValues = true;
+                functionTypeParameters = true;
+                parameterNames = true;
+                rangeVariableTypes = true;
+              };
+            };
+          };
+        };
         jdtls.enable = true;
         kotlin_language_server.enable = true;
         pylsp.enable = true;
@@ -394,6 +449,24 @@
         yamlls = {
           enable = true;
           filetypes = [ "yaml" ];
+          settings = {
+            yaml = {
+              keyOrdering = false; # -- disable alphabetic ordering of keys
+              schemas = {
+                kubernetes = "templates/**";
+                "http://json.schemastore.org/github-workflow" = ".github/workflows/*";
+                "http://json.schemastore.org/github-action" = ".github/action.{yml,yaml}";
+                "http://json.schemastore.org/prettierrc" = ".prettierrc.{yml,yaml}";
+                "http://json.schemastore.org/kustomization" = "kustomization.{yml,yaml}";
+                "http://json.schemastore.org/chart" = "Chart.{yml,yaml}";
+                "https://json.schemastore.org/dependabot-2.0.json" = ".github/dependabot.{yml,yaml}";
+                "https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json" = "*gitlab-ci*.{yml,yaml}";
+                "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json" = "*api*.{yml,yaml}";
+                "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" = "*docker-compose*.{yml,yaml}";
+                "https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json" = "*flow*.{yml,yaml}";
+              };
+            };
+          };
         };
       };
     };
