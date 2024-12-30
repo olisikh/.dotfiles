20e1cc6b87f55252999cfa0bf8bd0997e01ef447
Oleksii Lisikh <alisiikh@gmail.com>
2024-12-31 00:22:08 +0100

    Delete nvim

--- nvim/after/plugin/fundo.lua
+++
@@ -1,5 +0,0 @@
-local fundo = require('fundo')
-fundo.setup({
-  archives_dir = os.getenv('HOME') .. '/.vim/fundo',
-  limit_archives_size = 512,
-})
--- nvim/after/plugin/oil.lua
+++
@@ -1,9 +0,0 @@
-local nmap = require('user.utils').nmap
-local oil = require('oil')
-
-oil.setup({})
-
-nmap('-', oil.open, { desc = 'oil: open parent directory' })
-nmap('_', function()
-  oil.open(vim.uv.cwd())
-end, { desc = 'oil: open cwd directory' })
