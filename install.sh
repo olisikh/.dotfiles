938cf9aea21c039c220be74ebc0c0b1c73a2e640
Oleksii Lisikh <alisiikh@gmail.com>
2026-01-15 22:02:03 +0100

    fix uninstall.sh script, remove old readme text

--- install.sh
+++ install.sh
@@ -8,10 +8,14 @@ then
     echo "Nix is missing, install Determinate Nix before proceeding."
 fi
 
-# NOTE: if the LocalHostName value is wrong, do `sudo scutil --set LocalHostName <your_hostname>`
+# NOTE: if the LocalHostName value is wrong:
+# sudo scutil --set LocalHostName <hostname>
+# sudo scutil --set HostName <hostname>
 HOSTNAME=$(/usr/sbin/scutil --get LocalHostName)
 
 # download the internet and install flake
-sudo nix build .#darwinConfigurations.${HOSTNAME}.system --show-trace --print-build-logs -vvv
-./result/sw/bin/darwin-rebuild switch --flake .
+sudo nix build .#darwinConfigurations.${HOSTNAME}.system --show-trace --print-build-logs -v
+
+# activate nix-darwin and home-manager
+sudo ./result/sw/bin/darwin-rebuild switch --flake .
 
