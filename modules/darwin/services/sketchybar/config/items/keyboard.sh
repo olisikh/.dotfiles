29995cdcd2626e25bcf17366e8131b19c6a4aa10
Oleksii Lisikh <alisiikh@gmail.com>
2026-01-22 08:51:10 +0100

    feat/keyboard: Implement a keyboard item for sketchybar (#10)
    * keyboard plugin for sketchybar
    
    * fix subscribe call for keyboard plugin
    
    * shorten path to script used in pomodoro.sh item
    
    ---------
    
    Co-authored-by: Oleksii Lisikh <>

---
+++ modules/darwin/services/sketchybar/config/items/keyboard.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+sketchybar --add event keyboard_layout_change com.apple.Carbon.TISNotifySelectedKeyboardInputSourceChanged
+
+COLOR="$WHITE"
+
+sketchybar --add item keyboard right \
+	--set keyboard \
+	icon=" " \
+	icon.color="$COLOR" \
+	icon.padding_left=10 \
+	label.color="$COLOR" \
+	label.padding_right=10 \
+	label.width=30 \
+	background.height=26 \
+	background.corner_radius="$CORNER_RADIUS" \
+	background.padding_right=5 \
+	background.border_width="$BORDER_WIDTH" \
+	background.border_color="$COLOR" \
+	background.color="$BAR_COLOR" \
+	background.drawing=on \
+	script="$PLUGIN_DIR/keyboard.sh" \
+	--subscribe keyboard keyboard_layout_change
--- modules/darwin/services/sketchybar/config/plugins/power.sh
+++ modules/darwin/services/sketchybar/config/plugins/power.sh
@@ -9,18 +9,18 @@ fi
 
 case ${PERCENTAGE} in
 9[0-9] | 100)
-	ICON=""
+	ICON=" "
 	;;
 [6-8][0-9])
-	ICON=""
+	ICON=" "
 	;;
 [3-5][0-9])
-	ICON=""
+	ICON=" "
 	;;
 [1-2][0-9])
-	ICON=""
+	ICON=" "
 	;;
-*) ICON="" ;;
+*) ICON=" " ;;
 esac
 
 if [ "$CHARGING" != "" ]; then
