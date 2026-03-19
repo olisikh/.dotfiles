6fcf1fee5166a3eb9e4fcb624290e1384a48f159
Oleksii Lisikh <alisiikh@gmail.com>
2026-03-19 11:35:53 +0100

    Add mac mini

---
+++ homes/aarch64-darwin/olisikh@olisikh-mini/default.nix
@@ -0,0 +1,38 @@
+{ lib, namespace, ... }:
+let
+  inherit (lib.${namespace}) enabled disabled;
+in
+{
+  olisikh = {
+    direnv = enabled;
+    zsh = enabled;
+    fzf = enabled;
+    zoxide = enabled;
+    bat = enabled;
+    wezterm = enabled;
+    ripgrep = enabled;
+    starship = enabled;
+    git = enabled;
+    yazi = enabled;
+    nixvim = enabled;
+    intellij-idea = {
+      enable = true;
+      plugins = [
+        "com.github.catppuccin.jetbrains"
+        "com.github.catppuccin.jetbrains_icons"
+        "org.intellij.scala"
+        "org.jetbrains.kotlin"
+        "org.intellij.plugins.hcl"
+        "Docker"
+        "IdeaVIM"
+        "Lombook Plugin"
+        "youngstead.relative-line-numbers"
+      ];
+    };
+    opencode = enabled;
+    # codex = enabled;
+    # claude-code = enabled;
+    user.personal = enabled;
+    sops = enabled;
+  };
+}
---
+++ systems/aarch64-darwin/olisikh-mini/default.nix
@@ -0,0 +1,47 @@
+{ lib, namespace, pkgs, ... }:
+let
+  inherit (lib.${namespace}) enabled disabled;
+
+  username = "olisikh";
+  hostName = "olisikh-mini";
+  localHostName = hostName;
+in
+{
+  olisikh = {
+    # NOTE: Install Determinate Nix, don't rely on Darwin to manage Nix
+    nix = disabled;
+
+    user = {
+      enable = true;
+      inherit username;
+    };
+    homebrew.personal = enabled;
+    services = {
+      jankyborders = enabled;
+      yabai = enabled;
+      sketchybar = enabled;
+      skhd = enabled;
+      colima = enabled;
+      ollama = enabled;
+    };
+  };
+
+  networking = {
+    inherit hostName localHostName;
+
+    computerName = "Oleksii's Mac Mini";
+  };
+
+  environment = {
+    systemPackages = with pkgs; [
+      cocoapods
+    ];
+  };
+
+  system = {
+    primaryUser = username;
+
+    # nix-darwin state version, DO NOT TOUCH!
+    stateVersion = 6;
+  };
+}
