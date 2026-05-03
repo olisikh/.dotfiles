815dfb64dcebb78e9c5b10d15fa9c228da59bcbd
Oleksii Lisikh <oleksii.lisikh@gmail.com>
2026-05-03 23:55:06 +0200

    enable auto-up tailscale launchd daemon

--- modules/darwin/network/tailscale/default.nix
+++ modules/darwin/network/tailscale/default.nix
@@ -1,9 +1,23 @@
-{ lib, config, namespace, ... }:
+{ lib, config, namespace, pkgs, ... }:
 let
   inherit (lib) mkIf;
   inherit (lib.${namespace}) mkBoolOpt;
 
   cfg = config.${namespace}.network.tailscale;
+  tailscalePkg = config.services.tailscale.package;
+  tailscaleUp = pkgs.writeShellScript "tailscale-up" ''
+    set -eu
+
+    for _ in $(seq 1 60); do
+      if ${tailscalePkg}/bin/tailscale up; then
+        exit 0
+      fi
+
+      sleep 2
+    done
+
+    ${tailscalePkg}/bin/tailscale up
+  '';
 in
 {
   options.${namespace}.network.tailscale = {
@@ -12,5 +26,24 @@ in
 
   config = mkIf cfg.enable {
     services.tailscale.enable = true;
+
+    launchd.daemons.tailscale-up = {
+      path = [ config.environment.systemPath ];
+
+      serviceConfig = {
+        Label = "com.tailscale.up";
+        ProgramArguments = [ "${tailscaleUp}" ];
+
+        RunAtLoad = true;
+        StartInterval = 30;
+        KeepAlive = {
+          NetworkState = true;
+          SuccessfulExit = false;
+        };
+
+        StandardOutPath = "/var/log/tailscale-up.log";
+        StandardErrorPath = "/var/log/tailscale-up.log";
+      };
+    };
   };
 }
