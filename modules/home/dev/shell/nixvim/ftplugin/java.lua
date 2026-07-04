433ea8c6969cfef1dfb533cb8c75840f006f403a
Oleksii Lisikh <oleksii.lisikh@gmail.com>
2026-07-04 23:53:57 +0200

    add more useful jdtls keymaps

--- modules/home/dev/shell/nixvim/ftplugin/java.lua
+++ modules/home/dev/shell/nixvim/ftplugin/java.lua
@@ -3,9 +3,49 @@ vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.expandtab = true
 
-vim.keymap.set("n", "<leader>co", function()
-  vim.lsp.buf.code_action({
-    context = { only = { "source.organizeImports" } },
-    apply = true,
-  })
-end, { buffer = true, desc = "java: [o]rganize imports" })
+local function java_action(kind, desc)
+	return function()
+		vim.lsp.buf.code_action({
+			context = { only = { kind } },
+			apply = true,
+		})
+	end
+end
+
+local map_opts = { buffer = true, silent = true }
+
+vim.keymap.set(
+	"n",
+	"<leader>co",
+	java_action("source.organizeImports"),
+	vim.tbl_extend("force", map_opts, { desc = "java: [o]rganize imports" })
+)
+vim.keymap.set(
+	{ "n", "v" },
+	"<leader>cev",
+	java_action("refactor.extract.variable"),
+	vim.tbl_extend("force", map_opts, { desc = "java: [e]xtract [v]ariable" })
+)
+vim.keymap.set(
+	{ "n", "v" },
+	"<leader>cec",
+	java_action("refactor.extract.constant"),
+	vim.tbl_extend("force", map_opts, { desc = "java: [e]xtract [c]onstant" })
+)
+vim.keymap.set(
+	{ "n", "v" },
+	"<leader>cem",
+	java_action("refactor.extract.method"),
+	vim.tbl_extend("force", map_opts, { desc = "java: [e]xtract [m]ethod" })
+)
+vim.keymap.set(
+	{ "n", "v" },
+	"<leader>ci",
+	java_action("refactor.inline"),
+	vim.tbl_extend("force", map_opts, { desc = "java: [i]nline" })
+)
+vim.keymap.set("n", "<leader>cg", function()
+	vim.lsp.buf.code_action({
+		context = { only = { "source.generate" } },
+	})
+end, vim.tbl_extend("force", map_opts, { desc = "java: [g]enerate" }))
