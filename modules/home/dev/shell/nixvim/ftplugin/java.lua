ffd927cffcb17c4b4561e82ecaf89b783f6d1354
Oleksii Lisikh <oleksii.lisikh@gmail.com>
2026-07-04 23:51:26 +0200

    add organize imports command

--- modules/home/dev/shell/nixvim/ftplugin/java.lua
+++ modules/home/dev/shell/nixvim/ftplugin/java.lua
@@ -2,3 +2,10 @@ vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.expandtab = true
+
+vim.keymap.set("n", "<leader>co", function()
+  vim.lsp.buf.code_action({
+    context = { only = { "source.organizeImports" } },
+    apply = true,
+  })
+end, { buffer = true, desc = "java: [o]rganize imports" })
