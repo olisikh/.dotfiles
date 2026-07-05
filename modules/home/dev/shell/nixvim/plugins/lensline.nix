d700c9a771d3095a0f432cd00b86b5e30ace0d7b
Oleksii Lisikh <oleksii.lisikh@gmail.com>
2026-07-05 02:39:44 +0200

    change the style of lensline text

--- modules/home/dev/shell/nixvim/plugins/lensline.nix
+++ modules/home/dev/shell/nixvim/plugins/lensline.nix
@@ -9,9 +9,24 @@
           style = {
             placement = "inline";
             prefix = "";
+            highlight = "LenslineText";
           };
         }
       ];
     };
   };
+
+  extraConfigLua = ''
+    -- Change the style of lens text (inspired by gitsigns current_line_blame).
+    local function lensline_apply_highlight()
+      vim.api.nvim_set_hl(0, "LenslineText", { fg = "#45475a" })
+    end
+
+    lensline_apply_highlight()
+
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      group = vim.api.nvim_create_augroup("LenslineHighlight", { clear = true }),
+      callback = lensline_apply_highlight,
+    })
+  '';
 }
